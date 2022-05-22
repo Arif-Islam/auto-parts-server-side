34326858696d110d5e06e9dbc2c29a784dbb5274
Arif <iariful950@gmail.com>
2022-05-22 15:00:34 +0600

    server side inititalized!

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from server side!');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
