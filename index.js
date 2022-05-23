29d56ca114579d2aa82dc75ffc71af646ce6a1db
Arif <iariful950@gmail.com>
2022-05-23 06:02:43 +0600

    parts loaded on homepage

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymvsg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('auto-parts').collection('parts');
+
+        // load all parts
+        app.get('/parts', async (req, res) => {
+            // const query = {};
+            const result = await partsCollection.find().toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from server side!');
